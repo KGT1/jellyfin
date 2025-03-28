be81a37fb10f2b3e2384c9576933504dfe7f68c8
KGT1 <kilian.gamn@gmx.de>
2025-03-28 15:54:55 +0100

    remove API mentions from BrandingOptions

--- MediaBrowser.Model/Branding/BrandingOptions.cs
+++ MediaBrowser.Model/Branding/BrandingOptions.cs
@@ -1,14 +1,8 @@
-using System.Text.Json.Serialization;
-
 namespace MediaBrowser.Model.Branding;
 
 /// <summary>
 /// The branding options.
 /// </summary>
-/// <remarks>
-/// This class is for internal server use. For API operations, use <see cref="BrandOptionsDto"/> instead
-/// to prevent unintended updates to the SplashscreenLocation property.
-/// </remarks>
 public class BrandingOptions
 {
     /// <summary>
@@ -32,9 +26,7 @@ public class BrandingOptions
     /// Gets or sets the splashscreen location on disk.
     /// </summary>
     /// <remarks>
-    /// Not served via the API.
     /// Only used to save the custom uploaded user splashscreen in the configuration file.
     /// </remarks>
-    [JsonIgnore]
     public string? SplashscreenLocation { get; set; }
 }
